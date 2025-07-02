bd94cc47ce0ef944190fe71f206776fb378ba215
João Araújo <102301547+joao0araujo@users.noreply.github.com>
2025-07-02 19:11:20 -0300

    Adicionando search_bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/header/header'
 import Bottom from './components/bottom/bottom'
+import Search from './components/search_bar/search'
 
  export default function App() {
 
   return (
     <div className="app-container"> 
       <Header />
+      <Search />
       <Bottom />
     </div>
   )
--- frontend/src/components/search_bar/search.tsx
+++ frontend/src/components/search_bar/search.tsx
@@ -4,8 +4,11 @@ import lupa from '../../assets/Lupa.png'
 
 export default function Search(){
     return(
-        <div>
-
+        <div className={styles.container}>
+            <div className={styles.searchBar}>
+                <input placeholder="Digite aqui para exibir as sugestões"/>
+                <button > <img src={lupa} alt="Lupa" /> </button>
+            </div>
         </div>
     )
 }
