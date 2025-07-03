533f58b79ae476ad7e3cfae9b135765ba390ce66
João Araújo <102301547+joao0araujo@users.noreply.github.com>
2025-07-03 11:02:29 -0300

    Integrando search_bar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+
+    <ApolloProvider client={client}>
+
+      <App />
+
+    </ApolloProvider>
+
   </StrictMode>,
 )
